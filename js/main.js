1aec758e5387f1241bf3f91511412827efa57c47
Graphic <graphic@SOD-SJH-516-32M.local>
2015-10-02 15:04:15 -0400

    Switch

--- js/main.js
+++ js/main.js
@@ -1,2 +1,21 @@
+//$(document).ready(function(){});
+
+	//var switchStatus = true;
+
+	//$(document).on('click', 'button', function)
+
+//if ('.light').on('click', function(e){
+	//$('.switch').toggleClass('on');
+
+//});
 
 
+
+
+
+
+$('.switch').on('click', function(e){
+	$('body').toggleClass('dark').toggleClass('light');
+	$('button').toggleClass('off').toggleClass('on');
+	$('.status').text('Hey, who turn off the lights?');
+});
